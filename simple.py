fdf71aa8f9bb5e644cc415d3f00b4c03d0c9e322
asatyana5 <164791943+asatyana5@users.noreply.github.com>
2025-09-03 17:37:52 +0530

    Update simple.py

--- simple.py
+++ simple.py
@@ -2,7 +2,7 @@ import sys
 
 def main():
     print("❌ Test Failed (forced failure for Jenkins test)!")
-    sys.exit(1)   # non-zero exit code → Jenkins marks build as failed
+    sys.exit(0)   # non-zero exit code → Jenkins marks build as failed
 
 if __name__ == "__main__":
     main()
